f2623067e0f17636b771da5bfe8167050f3cc8bc
8891689 <170772332+8891689@users.noreply.github.com>
2025-07-30 07:47:36 +0800

    Update ripemd160_avx.h

--- ripemd160_avx.h
+++ ripemd160_avx.h
@@ -1,11 +1,25 @@
-//author： https://github.com/8891689
+/* Apache License, Version 2.0
+   Copyright [2025] [8891689]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   Author: 8891689 (https://github.com/8891689)
+*/
 #ifndef RIPEMD160_avx_H
 #define RIPEMD160_avx_H
 
 #include <immintrin.h> 
 #include <stdint.h>    
 #include <stddef.h>    
-
 
 #define LANE_COUNT 8
 #define BLOCK_SIZE 64
